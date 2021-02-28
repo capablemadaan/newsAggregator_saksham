368fb3fcfc7c900bc73bec032d3efc1c2582b130
capablemadaan <77107541+capablemadaan@users.noreply.github.com>
2021-02-28 17:05:02 +0530

    Initial commit

---
+++ news/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+import requests
+from bs4 import BeautifulSoup
+
+toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
+toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+toi_headings = toi_soup.find_all('h2')
+toi_headings=toi_headings[2:-13]
+toi_news=[]
+for th in toi_headings:
+    toi_news.append(th.text)
+
+ht_r = requests.get("https://www.tribuneindia.com/news/nation")
+ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
+ht_headings = ht_soup.findAll("h4", {"class": "ts-card-title"})
+#ht_headings = ht_headings[2:]
+ht_news = []
+for hth in ht_headings:
+    ht_news.append(hth.text)
+
+def index(req):
+    return render(req, 'news/index.html',{'toi_news':toi_news, 'ht_news': ht_news})
