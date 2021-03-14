76300d613da3fb6b6e5f5cc9da5f9a60d0961671
capablemadaan <77107541+capablemadaan@users.noreply.github.com>
2021-03-14 10:01:12 +0530

    a

--- NewsAggregator/urls.py
+++ NewsAggregator/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('homeaffairs/', views.index2),
     path('fa/', views.index3),
     path('sports/', views.index4),
-#     path('snt/', views.index5),
+    path('snt/', views.index5),
     path('busi/', views.index6),
     path('dtu/', views.index7)       
  ]
