fa6a447e86a4c33363300dfd26756ec6f207d850
capablemadaan <77107541+capablemadaan@users.noreply.github.com>
2021-04-12 13:19:59 +0530

    sa

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsAggregator.settings')
-    from django.core.management import execute_from_command_line
     
     
 if __name__ == '__main__':
