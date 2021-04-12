77a5021bc7aa672c9120c689819a9af3d6ca36fa
capablemadaan <77107541+capablemadaan@users.noreply.github.com>
2021-04-12 13:14:31 +0530

    a

--- manage.py
+++ manage.py
@@ -9,7 +9,6 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsAggregator.settings')
     try:
         from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
